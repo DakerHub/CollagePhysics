7f07b1ee7f5b12ebb182cd484f9f2a59654c2dc0
冯陈诚 <523246656@qq.com>
2017-05-03 00:37:48 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>University physics</title>
+    <link rel="stylesheet" href="wp-content/themes/collagePhysics/style.css">
+</head>
+<body>
+    <header class="wp-header">
+        <div class="header">
+            <div class="wp-logo">
+                <img src="wp-content/themes/collagePhysics/images/logo.png" alt="South China University of Technology">
+            </div>
+            <h1 class="h1">Physics</h1>
+        </div>
+        <nav class="wp-nav">
+            <ul class="nav">
+                <li><a href="#" class="active">Home</a></li>
+                <li><a href="#">Curriculum resources</a></li>
+                <li><a href="#">Announcement</a></li>
+                <li><a href="#">Physics news</a></li>
+                <li><a href="#">About</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main></main>
+    <footer></footer>
+</body>
+</html>
